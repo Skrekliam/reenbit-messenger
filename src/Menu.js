36e001c46fdc18ce047bdc1e91de38d20cc4387f
Skrekliam <typnickman@gmail.com>
2021-07-20 14:26:20 +0300

    Add components\

---
+++ src/Menu.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./menu.scss";
+
+function Menu() {
+  return (
+    <div>
+      <div className="head">
+        <div className="avatar">
+          
+        </div>
+        <div className="search"></div>
+      </div>
+
+      <div className="chats">
+        <div className="chat"></div>
+        <div className="chat"></div>
+        <div className="chat"></div>
+      </div>
+      {/* left side */}
+      {/* avatar */}
+      {/* search chat */}
+
+      {/* chats */}
+      {/* chat item */}
+    </div>
+  );
+}
+
+export default Menu;
