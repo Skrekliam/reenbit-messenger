3d03e53d2425d598ddb6687fe999f2249cd3e22c
Skrekliam <typnickman@gmail.com>
2021-07-21 13:37:32 +0300

    Add header section for messages

--- src/Message.js
+++ src/Message.js
@@ -4,7 +4,12 @@ import "./message.scss";
 function Message() {
   return (
     <div className="messageBlock">
-      {/* chat avatar + name */}asd
+      {/* chat avatar + name */}
+      <div className="message__header">
+          <img src="./imgs/avatar.png" alt="Avatar" className="avatarImg" /> <p>Josefina</p>
+      </div>
+      <div className="message__text"></div>
+      <div className="message__send"></div>
       {/* message */}
     </div>
   );
