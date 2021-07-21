51ac29bd8430f2380c461f7ce505c4cc3ec73f91
Skrekliam <typnickman@gmail.com>
2021-07-21 13:14:48 +0300

    Add messages section

--- src/Menu.js
+++ src/Menu.js
@@ -3,13 +3,13 @@ import "./menu.scss";
 
 function Menu() {
   return (
-    <div>
+    <div className="menu">
       <div className="head">
         <div className="avatar">
           <img
             src="./imgs/avatar.png"
             alt="Avatar"
-            className="head__avatarImg"
+            className="avatarImg"
           />
         </div>
         <div className="search">
@@ -21,9 +21,29 @@ function Menu() {
       </div>
 
       <div className="chats">
-        <div className="chat"></div>
-        <div className="chat"></div>
-        <div className="chat"></div>
+        <h1 className="chatsHeader">Chats</h1>
+        <div className="chat">
+          <div className="messageSection">
+        <img src="./imgs/avatar.png" alt="Avatar" className="avatarImg" />
+          <div className="text">
+            <p className="contact">Alice Freeman</p>
+            <p className="message">You are the worst!</p>
+          </div>
+          </div>
+          <div className="time">Jun 12, 2017</div>
+        </div>
+          
+        <div className="chat">
+          <div className="messageSection">
+        <img src="./imgs/avatar.png" alt="Avatar" className="avatarImg" />
+          <div className="text">
+            <p className="contact">Josefina</p>
+            <p className="message">We are losing money! Quick!</p>
+          </div>
+          </div>
+          <div className="time">Feb 18, 2017</div>
+        </div>
+
       </div>
       {/* left side */}
       {/* avatar */}
