e2af7543bf840503c1217a07fee3aff3f998bdf3
Skrekliam <typnickman@gmail.com>
2021-07-22 00:09:01 +0300

    Fix showing big text messages

--- src/components/ChatItem.js
+++ src/components/ChatItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ChatItem({chat}) {
     const date = new Date()
-    const time = new Date(chat?.chat.lastUpdated.seconds*1000);
+    const time = new Date(chat?.chat.lastUpdated*1000);
     let messageTime = date - time - 86400000 < 0 ? time.getHours() +':'+ ('0' + time.getMinutes()).substr(-2) +':' +('0' + time.getSeconds()).substr(-2) : time.getDate() + '/' + (time.getMonth() + 1) + '/' + (time.getYear() + 1900); 
     console.log(new Date())
     console.log(chat)
@@ -12,7 +12,7 @@ function ChatItem({chat}) {
             <img src="./imgs/avatar.png" alt="Avatar" className="avatarImg" />
             <div className="text">
               <p className="contact">{chat?.chat.user1}</p>
-              <p className="message">{chat?.chat.lastMessage}</p>
+              <p className="message">{chat?.chat.lastMessage.length > 81 ? chat?.chat.lastMessage.slice(0,81) + '...' : chat?.chat.lastMessage }</p>
             </div>
           </div>
           <div className="time">{messageTime}</div>
