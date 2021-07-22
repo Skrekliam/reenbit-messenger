e37cf36f0e8526e4d3947ecbfd177ca3649489fe
Skrekliam <typnickman@gmail.com>
2021-07-22 21:22:51 +0300

    Styling fix

--- src/components/MessageSend.js
+++ src/components/MessageSend.js
@@ -3,15 +3,16 @@ import { db } from "./firebase";
 import firebase from "firebase";
 import generateMessage from "./GenerateMessage";
 
-function MessageSend({ recepient, user, chatId,addNewMessages }) {
+function MessageSend({ recepient, user, chatId, addNewMessages }) {
   const [message, setMessage] = useState("");
-  const handleSend = () => {
+  const handleSend = (e) => {
+    e.preventDefault();
     db.collection("chats")
       .doc(chatId)
       .update({
         lastMessage: message,
         lastUpdated: firebase.firestore.FieldValue.serverTimestamp(),
-        lastSender: user.displayName
+        lastSender: user.displayName,
       })
       .then(() =>
         db.collection("chats").doc(chatId).collection("messages").add({
@@ -20,22 +21,24 @@ function MessageSend({ recepient, user, chatId,addNewMessages }) {
           timestamp: firebase.firestore.FieldValue.serverTimestamp(),
         })
       )
-      .then(() =>{
-        setMessage("")
-        generateMessage(chatId,recepient,addNewMessages)
+      .then(() => {
+        setMessage("");
+        generateMessage(chatId, recepient, addNewMessages);
       });
   };
   return (
     <div className="input-group">
-      <button className="icon" onClick={handleSend}>
-        <img src="./imgs/paper-plane.svg" alt="send" />
-      </button>
-      <input
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        type="text"
-        placeholder="Type your message"
-      />
+      <form onSubmit={handleSend}>
+        <button className="icon" onClick={handleSend}>
+          <img src="./imgs/paper-plane.svg" alt="send" />
+        </button>
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          type="text"
+          placeholder="Type your message"
+        />
+      </form>
     </div>
   );
 }
